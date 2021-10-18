d63c10b045b500a3e3d6e17e877af66f26572836
Daniel Leon <daniel.radialista@gmail.com>
2021-10-18 11:53:38 -0300

    iniciando o projeto

---
+++ models/connection.js
@@ -0,0 +1,22 @@
+// criando a conexão com o banco de dados
+require('dotenv').config();
+
+const { MongoClient } = require('mongodb');
+
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+const connection = () =>
+  MongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+
+module.exports = connection;
